918a14e40b7389ee08c0d1789ec9d0996a9367dc
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-07 13:50:52 -0400

    fix: update icon generation for Realms Launcher

--- program_info/genicons.sh
+++ program_info/genicons.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-LAUNCHER_APPID="org.prismlauncher.PrismLauncher"
+LAUNCHER_APPID="org.realmslauncher.RealmsLauncher"
+LAUNCHER_NAME="RealmsLauncher"
 
 svg2png() {
     input_file="$1"
@@ -15,24 +16,24 @@ if command -v "inkscape" && command -v "icotool" && command -v "oxipng"; then
     # Windows ICO
     d=$(mktemp -d)
 
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_16.png" 16 16
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_24.png" 24 24
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_32.png" 32 32
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_48.png" 48 48
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_64.png" 64 64
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_128.png" 128 128
-    svg2png ${LAUNCHER_APPID}.svg "$d/prismlauncher_256.png" 256 256
+    svg2png ${LAUNCHER_APPID}.svg "$d/${LAUNCHER_NAME}_16.png" 16 16
+    svg2png ${LAUNCHER_APPID}.svg "$d/${LAUNCHER_NAME}_24.png" 24 24
+    svg2png ${LAUNCHER_APPID}.svg "$d/${LAUNCHER_NAME}_32.png" 32 32
+    svg2png ${LAUNCHER_APPID}.svg "$d/${LAUNCHER_NAME}_48.png" 48 48
+    svg2png ${LAUNCHER_APPID}.svg "$d/${LAUNCHER_NAME}_64.png" 64 64
+    svg2png ${LAUNCHER_APPID}.svg "$d/${LAUNCHER_NAME}_128.png" 128 128
+    svg2png ${LAUNCHER_APPID}.svg "$d/${LAUNCHER_NAME}_256.png" 256 256
 
-    oxipng --opt max --strip all --alpha --interlace 0 "$d/prismlauncher_"*".png"
+    oxipng --opt max --strip all --alpha --interlace 0 "$d/${LAUNCHER_NAME}_"*".png"
 
-    rm prismlauncher.ico && icotool -o prismlauncher.ico -c \
-        "$d/prismlauncher_256.png"  \
-        "$d/prismlauncher_128.png"  \
-        "$d/prismlauncher_64.png"   \
-        "$d/prismlauncher_48.png"   \
-        "$d/prismlauncher_32.png"   \
-        "$d/prismlauncher_24.png"   \
-        "$d/prismlauncher_16.png"
+    rm -f ${LAUNCHER_NAME}.ico && icotool -o ${LAUNCHER_NAME}.ico -c \
+        "$d/${LAUNCHER_NAME}_256.png" \
+        "$d/${LAUNCHER_NAME}_128.png" \
+        "$d/${LAUNCHER_NAME}_64.png" \
+        "$d/${LAUNCHER_NAME}_48.png" \
+        "$d/${LAUNCHER_NAME}_32.png" \
+        "$d/${LAUNCHER_NAME}_24.png" \
+        "$d/${LAUNCHER_NAME}_16.png"
 else
     echo "ERROR: Windows icons were NOT generated!" >&2
     echo "ERROR: requires inkscape, icotool and oxipng in PATH"
@@ -41,9 +42,7 @@ fi
 if command -v "inkscape" && command -v "iconutil" && command -v "oxipng"; then
     # macOS ICNS
     d=$(mktemp -d)
-
-    d="$d/prismlauncher.iconset"
-
+    d="$d/${LAUNCHER_NAME}.iconset"
     mkdir -p "$d"
 
     svg2png ${LAUNCHER_APPID}.bigsur.svg "$d/icon_16x16.png" 16 16
@@ -60,11 +59,11 @@ if command -v "inkscape" && command -v "iconutil" && command -v "oxipng"; then
     oxipng --opt max --strip all --alpha --interlace 0 "$d/icon_"*".png"
 
     iconutil -c icns "$d"
-    cp -v "$d/prismlauncher.icns" .
+    cp -v "$d/${LAUNCHER_NAME}.icns" .
 else
     echo "ERROR: macOS icons were NOT generated!" >&2
     echo "ERROR: requires inkscape, iconutil and oxipng in PATH"
 fi
 
-# replace icon in themes
+# Keep the launcher theme icon in sync with the program branding.
 cp -v ${LAUNCHER_APPID}.svg "../launcher/resources/multimc/scalable/launcher.svg"

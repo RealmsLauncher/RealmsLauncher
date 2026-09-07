8d48edb1d9f91777b8c9b753d786f82ed4969843
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-07 12:03:20 -0400

    Update Application.cpp

--- launcher/Application.cpp
+++ launcher/Application.cpp
@@ -1351,7 +1351,7 @@ void Application::performMainStartupAction()
         QString storedKey = m_settings->get("LicenseKey").toString();
         bool keyValid = false;
         if (!storedKey.isEmpty()) {
-            keyValid = m_licenseManager->validate(storedKey);
+            keyValid = m_licenseManager->validateLicense(storedKey);
         }
 
         if (!keyValid) {
@@ -1379,7 +1379,7 @@ void Application::performMainStartupAction()
                     QMessageBox::warning(nullptr, tr("Invalid Input"),
                         tr("License key cannot be empty. Please enter your key."));
                     ok = false; // loop again
-                } else if (!m_licenseManager->validate(enteredKey)) {
+                } else if (!m_licenseManager->validateLicense(enteredKey)) {
                     QMessageBox::warning(nullptr, tr("Invalid License"),
                         tr("The license key you entered is invalid.\n"
                            "Please check and try again."));
